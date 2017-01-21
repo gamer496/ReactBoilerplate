837438d297c945e8cff09f5dba83d5dd6e830985
gamer496 <singhjasdeep496@gmail.com>
2017-01-21 19:07:12 +0530

    updated boilerplate with foundation scss jquery and routers

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+
+  externals: {
+    jquery: 'jQuery'
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+
   output: {
     path: __dirname,
     filename: './public/bundle.js'
@@ -8,6 +26,8 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
+      Main: 'app/components/Main.jsx',
+      applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
@@ -23,5 +43,11 @@ module.exports = {
         exclude: /(node_modules|bower_components)/
       }
     ]
-  }
+  },
+
+  devtool: 'cheap-module-eval-source-map'
 };
+
+/*note I think this is a bug with foundation*/
+/*if after npm install webpack fails to find foundation directory run*/
+/*npm i -D css-loader@0.23.1 script-loader@0.6.1 style-loader@0.13.0 jquery@2.2.1 foundation-sites@6.2.0*/
